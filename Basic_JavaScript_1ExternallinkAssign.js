6e358ee181f8d64c9d0db5cd3a3be492042bfdfa
Bonnie Mack <78276424+bonnmccu@users.noreply.github.com>
2021-02-10 10:08:06 -0700

    Add files via upload

---
+++ Basic_JavaScript_1ExternallinkAssign.js
@@ -0,0 +1,9 @@
+alert("Hello, World!"); 
+	<script>
+	function My_First_Function() {
+		var String = "Kiss Me I'm Irish!";
+		var result = String.fontcolor("green');
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
+	</script>
